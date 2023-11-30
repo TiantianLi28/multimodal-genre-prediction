20814fa7a806d3e618678d3d3714e4b13c37080c
espkz <ellie.paek@gmail.com>
2023-11-29 21:20:56 -0500

    embeddings

--- preprocess/lyric_preprocess.py
+++ preprocess/lyric_preprocess.py
@@ -1,13 +1,33 @@
 import pandas as pd
 import numpy as np
-import nltk
+import nltk, spacy
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+
+def divide_lyrics(lyrics):
+    """ dividing lyrics"""
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(lyrics)
+    verses = []
+    current_verse = []
+
+    for token in doc:
+        if token.is_sent_start:
+            if current_verse:
+                verses.append(" ".join(current_verse))
+                current_verse = []
+        current_verse.append(token.text)
+
+    if current_verse:
+        verses.append(" ".join(current_verse))
+    return verses
+
 
 def preprocess_lyrics(lyrics):
     """
@@ -15,25 +35,8 @@ def preprocess_lyrics(lyrics):
     :param lyrics: str, lyrics
     :return: preprocessed string of lyrics
     """
-    lyrics = lyrics.split()
-    lyrics_list = []
-    lyric = ""
-    # split by capitalized words (not perfect)
-    for i in range(len(lyrics)):
-        word = lyrics[i]
-        a = word.istitle()
-        n = len(lyrics_list)
-        if (word.istitle()):
-            if lyric == "":
-                lyric = word
-            else:
-                lyrics_list.append(lyric)
-                lyric = word
-        else:
-            lyric += " " + word
-            if i + 1 == len(lyrics):
-                lyrics_list.append(lyric)
     processed_lyrics_list = []
+    lyrics_list = divide_lyrics(lyrics)
     for lyric in lyrics_list:
         words = word_tokenize(lyric)
 
@@ -45,7 +48,6 @@ def preprocess_lyrics(lyrics):
         lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
         processed_lyrics = ' '.join(lemmatized_words)
         processed_lyrics_list.append(processed_lyrics)
-
     return processed_lyrics_list
 
 def transform(lyrics):
@@ -59,13 +61,16 @@ def transform(lyrics):
 
     embeddings = model.encode(lyrics)
     average_embeddings = np.mean(embeddings, axis=1)
-    print(average_embeddings)
+    # print(average_embeddings)
     return average_embeddings
 
 if __name__ == "__main__":
-    df = pd.read_csv('dat/filtered_data.csv')
+    df = pd.read_csv('dat/data_with_genre713.csv')
     lyrics = df['lyrics']
-
-    test = df['lyrics'][0]
+    embeddings = []
+    for lyric in tqdm(lyrics):
+        embedding = transform(lyric)
+        embeddings.append(embedding)
+    df['lyric-embeddings'] = embeddings
+    df.to_csv('dataset_with_embeddings_2120.csv', index=False)
     # print(lyrics)
-    transform(test)
