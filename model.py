0ff352106fe22a97dbf1834bd316b8d7ac7a31fc
espkz <ellie.paek@gmail.com>
2023-11-30 01:38:27 -0500

    fix

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ import re
 HIDDEN_PARAMS = [(100,100,100), (100,80,60), (150, 120, 100), (150,100)]
 ALPHA = [1e-3, 1e-4, 1e-5]
 ESTIMATORS = [50,100,150,200]
-MAX_DEPTH =[3,5,7,10]
-MIN_LEAF = [25,50,75,100]
+MAX_DEPTH =[3,5,7]
+MIN_LEAF = [50,75,100]
 
 def metrics(model, x, y):
     prediction = model.predict(x)
@@ -34,7 +34,7 @@ def run_MLP(trainx, trainy, testx, testy):
 
         nn = MLPClassifier()
 
-        grid_search = GridSearchCV(estimator=nn, param_grid=grid, scoring=['precision', 'recall'])
+        grid_search = GridSearchCV(estimator=nn, param_grid=grid, scoring='accuracy', verbose=3)
 
         grid_search.fit(x, y)
 
@@ -45,7 +45,7 @@ def run_MLP(trainx, trainy, testx, testy):
         results = {'best-hidden': besthidden, 'best-activation': bestactivation, 'best-alpha': bestalpha}
         return results
 
-    best_results = tune_nn(trainx, trainy, hidden_layers, activations, alphas)
+    best_results = tune_nn(trainx, trainy, HIDDEN_PARAMS, activations, ALPHA)
     hidden = best_results['best-hidden']
     activation = best_results['best-activation']
     alpha = best_results['best-alpha']
@@ -55,17 +55,15 @@ def run_MLP(trainx, trainy, testx, testy):
 
     # metrics for test
     test_metrics = metrics(nn, testx, testy)
+    test_metrics['best-params'] = best_results
     return test_metrics
 
 def run_RF(trainx, trainy, testx, testy):
-    num_estimators = []
-    max_depths = []
-    min_samples_leaf = []
     def tune_rf(x, y, num_estimators, dparams, lsparams):
         # 2a
         grid = {'n_estimators' : num_estimators, 'max_depth': dparams, 'min_samples_leaf': lsparams}
         rf = RandomForestClassifier()
-        grid_search = GridSearchCV(estimator=rf, param_grid=grid, scoring=['precision', 'recall'])
+        grid_search = GridSearchCV(estimator=rf, param_grid=grid, scoring='accuracy', verbose=3)
         grid_search.fit(x, y)
         best_estimator = grid_search.best_params_['n_estimators']
         best_depth = grid_search.best_params_['max_depth']
@@ -73,7 +71,7 @@ def run_RF(trainx, trainy, testx, testy):
         results = {'best-estimator' : best_estimator, 'best-depth': best_depth, 'best-leaf-samples': best_leaf_samples}
         return results
 
-    grid, best_results = tune_rf(trainx, trainy, num_estimators, max_depths, min_samples_leaf)
+    best_results = tune_rf(trainx, trainy, ESTIMATORS, MAX_DEPTH, MIN_LEAF)
     estimator = best_results['best-estimator']
     max_depth = best_results['best-depth']
     leaves = best_results['best-leaf-samples']
@@ -82,6 +80,7 @@ def run_RF(trainx, trainy, testx, testy):
     forest.fit(trainx, trainy)
     # metrics for test
     test_metrics = metrics(forest, testx, testy)
+    test_metrics['best-params'] = best_results
     return test_metrics
 
 if __name__ == "__main__":
@@ -103,7 +102,3 @@ if __name__ == "__main__":
     audio_indices = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
     audio_metadata_train = X_train.iloc[:, audio_indices].copy()
     audio_metadata_test = X_test.iloc[:, audio_indices].copy()
-    audio_metadata_train = audio_metadata_train.values
-    audio_metadata_test = audio_metadata_test.values
-
-    # run models
